355b5cd60f438dc2d0d5efcf0830e8aaced4220a
Diego Cananea <diegocananea@gmail.com>
2019-02-28 18:24:50 -0300

    Remove Kubernetes access variables in vars.tf

--- app/vars.tf
+++ app/vars.tf
@@ -18,22 +18,6 @@ variable "password" {
   description = "Kubernetes master password"
 }
 
-variable "client_certificate" {
-  description = "Kubernetes client certificate"
-}
-
-variable "client_key" {
-  description = "Kubernetes client key"
-}
-
-variable "cluster_ca_certificate" {
-  description = "Kubernetes cluster CA certificate"
-}
-
-variable "host" {
-  description = "Kubernetes host"
-}
-
 variable "DB" {
   description = "Database name"
 }
--- postgresql/vars.tf
+++ postgresql/vars.tf
@@ -30,20 +30,4 @@ variable "password" {
   description = "Kubernetes master password"
 }
 
-variable "client_certificate" {
-  description = "Kubernetes client certificate"
-}
-
-variable "client_key" {
-  description = "Kubernetes client key"
-}
-
-variable "cluster_ca_certificate" {
-  description = "Kubernetes cluster CA certificate"
-}
-
-variable "host" {
-  description = "Kubernetes host"
-}
-
 
